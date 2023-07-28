b8081b3319532ba9de053a9cb4c354e1bd428e14
dillonzellis <dillonzellis@gmail.com>
2023-07-27 21:58:39 -0400

    add shadow to page

--- app/page.tsx
+++ app/page.tsx
@@ -13,21 +13,23 @@ export default function Home() {
   return (
     <main className="tw-min-h-screen tw-bg-brandingBlue-100 tw-text-brandingNeutral-700 tw-text-base">
       <Container>
-        <div className="tw-bg-white">
-          <img
-            className="tw-mx-auto tw-py-8 tw-w-[875px]"
-            src="https://lscu.coop/member-engagement/imgs/header.png"
-          />
+        <div className="tw-shadow-xl tw-shadow-brandingBlue-700/20">
+          <div className="tw-bg-white">
+            <img
+              className="tw-mx-auto tw-py-8 tw-w-[875px]"
+              src="https://lscu.coop/member-engagement/imgs/header.png"
+            />
+          </div>
+          <Welcome bgColor="lightBlue" />
+          <Welcome2 bgColor="blue" />
+          <Assistance bgColor="white" />
+          <FreeTraining bgColor="lightBlue" />
+          <ExploitVid bgColor="blue" />
+          <MoreTraining bgColor="white" />
+          <InfoSight bgColor="lightBlue" />
+          <PolicyPro bgColor="blue" />
+          <BoardFunding bgColor="white" />
         </div>
-        <Welcome bgColor="lightBlue" />
-        <Welcome2 bgColor="blue" />
-        <Assistance bgColor="white" />
-        <FreeTraining bgColor="lightBlue" />
-        <ExploitVid bgColor="blue" />
-        <MoreTraining bgColor="white" />
-        <InfoSight bgColor="lightBlue" />
-        <PolicyPro bgColor="blue" />
-        <BoardFunding bgColor="white" />
       </Container>
     </main>
   );

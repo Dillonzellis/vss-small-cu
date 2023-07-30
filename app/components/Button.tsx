7adb2702250f9269b7b81b7bb789a845a83025dc
dillonzellis <dillonzellis@gmail.com>
2023-07-30 11:04:21 -0400

    add button styles

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
 
 const Button = ({ text, link }: ButtonProps) => {
   return (
-    <div>
+    <div className="tw-inline-flex">
       <a
         className="tw-capitalize tw-border-2 tw-bg-brandingNeutral-100 tw-text-brandingBlue-400 tw-font-semibold tw-border-brandingBlue-400  tw-rounded-lg tw-px-8 tw-py-2"
         href={link}

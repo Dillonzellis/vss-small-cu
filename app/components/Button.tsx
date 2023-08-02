bae4560caac88d574795e15ca614126ba9632dd0
dillonzellis <dillonzellis@gmail.com>
2023-08-02 15:16:17 -0400

    button styles rounded and text base

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ text, link }: ButtonProps) => {
   return (
     <div className="tw-inline-flex">
       <a
-        className="tw-capitalize tw-border-2 tw-bg-brandingNeutral-100 tw-text-brandingBlue-400 tw-font-semibold tw-border-brandingBlue-400  tw-rounded-lg tw-px-8 tw-py-2"
+        className="tw-capitalize tw-border-2 tw-bg-brandingNeutral-100 tw-text-brandingBlue-400 tw-text-base tw-font-semibold tw-border-brandingBlue-400 tw-rounded-full tw-px-8 tw-py-2"
         href={link}
       >
         {text}

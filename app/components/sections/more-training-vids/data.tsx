28c3835530ac2ca161e21bc2df691627d2b61e7d
dillonzellis <dillonzellis@gmail.com>
2023-08-15 14:47:26 -0400

    add cyber sec video

--- app/components/sections/more-training-vids/data.tsx
+++ app/components/sections/more-training-vids/data.tsx
@@ -9,7 +9,7 @@ export const trainingVids = [
     src: "https://lscu.coop/member-engagement/docs/zoom_9.mp4",
   },
   {
-    src: "https://www.youtube.com/embed/ScMzIvxBSi4",
+    src: "https://www.youtube.com/embed/gg5gPN1aTP0",
   },
   {
     src: "https://lscu.coop/member-engagement/docs/vids_0820/supv_comm_0820.mp4",

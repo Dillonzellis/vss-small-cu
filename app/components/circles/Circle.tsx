51aacf4b4af33b24b862c570c6d01afe3ed96c46
dillonzellis <dillonzellis@gmail.com>
2023-08-15 12:57:40 -0400

    adjust section shadow and circles italic text

--- app/components/circles/Circle.tsx
+++ app/components/circles/Circle.tsx
@@ -13,7 +13,7 @@ const Circle = ({ title, link, imgSrc, variant }: CircleProps) => {
   return (
     <a href={link} className={`magic-circles ${variantClass} tw-relative tw-translate-y-0 before:tw-absolute before:tw-rounded-full before:tw-opacity-0 hover:tw-translate-y-[-10px] hover:before:tw-opacity-100 tw-gap-4 tw-px-6 tw-rounded-full hover:tw-shadow-brandingBlue-700/40 tw-h-[14rem] tw-w-[14rem] tw-flex tw-flex-col tw-justify-center tw-items-center tw-shadow-lg`}>
       {imgSrc && <img className="tw-bg-white tw-p-1 tw-rounded-md tw-shadow-lg" src={imgSrc} />}
-      <div className={`tw-text-center ${variantText} tw-font-bold tw-text-lg`}>{title}</div>
+      <div className={`tw-text-center ${variantText} tw-italic tw-font-bold tw-text-lg`}>{title}</div>
     </a>
   )
 }
--- app/components/layout/Section.tsx
+++ app/components/layout/Section.tsx
@@ -10,7 +10,7 @@ const Section = ({ children, bgColor }: SectionProps) => {
 
   return (
     <section
-      className={`tw-my-3 tw-shadow-md md:tw-py-12 tw-py-8 tw-px-2 md:tw-px-8 ${colorClass || ""
+      className={`tw-my-4 tw-shadow-lg tw-shadow-slate-900/20 tw-rounded-md md:tw-py-12 tw-py-8 tw-px-2 md:tw-px-8 ${colorClass || ""
         }`.trim()}
     >
       {children}

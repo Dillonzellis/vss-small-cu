e836a7bb19310866c0b19c9fd744512203682227
dillonzellis <dillonzellis@gmail.com>
2023-08-17 10:21:46 -0400

    add two col items variant and update exploit vid section

--- app/components/layout/TwoCol.tsx
+++ app/components/layout/TwoCol.tsx
@@ -1,10 +1,22 @@
 type TwoColProps = {
   children: React.ReactNode;
+  items?: "center" | "end";
 };
 
-const TwoCol = ({ children }: TwoColProps) => {
+const TwoCol = ({ children, items = "center" }: TwoColProps) => {
+
+  let itemsClass;
+
+  if (items === "center") {
+    itemsClass = "tw-items-center"
+  }
+
+  if (items === "end") {
+    itemsClass = "tw-items-end"
+  }
+
   return (
-    <div className="tw-grid lg:tw-grid-cols-2 tw-gap-8 md:tw-gap-12 tw-items-center">
+    <div className={`tw-grid lg:tw-grid-cols-2 tw-gap-8 md:tw-gap-12 ${itemsClass}`}>
       {children}
     </div>
   );

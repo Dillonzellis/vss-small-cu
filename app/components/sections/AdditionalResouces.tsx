adf6f231f85331af045fff3f0be0c400839349ef
dillonzellis <dillonzellis@gmail.com>
2023-08-31 13:17:52 -0400

    add free training links to additional resources

--- app/components/sections/AdditionalResouces.tsx
+++ app/components/sections/AdditionalResouces.tsx
@@ -1,26 +1,57 @@
-import Section from "../layout/Section"
-import { Color } from "../utils/colorClasses"
-import Container from "../layout/Container"
+import Section from "../layout/Section";
+import { Color } from "../utils/colorClasses";
+import Container from "../layout/Container";
 
 type AdditionalResourcesProps = {
   bgColor: Color;
-}
+};
+
+const list = [
+  {
+    label: "Charter Considerations",
+    href: "../docs/Charter-Considerations.doc",
+  },
+  {
+    label: "CDFI Certification",
+    href: "https://www.cdfifund.gov/programs-training/certification/cdfi",
+  },
+  {
+    label: "Conflict of Interest Resolution",
+    href: "https://lscu.coop/member-engagement/docs/Conflict-of-Interest-Resolution-0920.pdf",
+  },
+  {
+    label: "FinCen.gov",
+    href: "https://www.fincen.gov/",
+  },
+  {
+    label: "FinCen CTR FAQs",
+    href: "https://www.fincen.gov/frequently-asked-questions-regarding-fincen-currency-transaction-report-ctr",
+  },
+  {
+    label: "FFIEC, BSA Manual",
+    href: "https://bsaaml.ffiec.gov/manual",
+  },
+];
 
 const AdditionalResources = ({ bgColor }: AdditionalResourcesProps) => {
   return (
     <Section bgColor={bgColor}>
       <Container>
         <div className="tw-text-brandingBlue-100">
-          <div className="tw-mx-auto md:tw-text-center tw-text-2xl md:tw-text-3xl lg:tw-text-4xl tw-pb-4 tw-font-medium">Additional Resources</div>
+          <div className="tw-mx-auto md:tw-text-center tw-text-2xl md:tw-text-3xl lg:tw-text-4xl tw-pb-4 tw-font-medium">
+            Additional Resources
+          </div>
           <ul className="tw-list-disc tw-list-inside lg:tw-text-lg">
-            <li><a href="../docs/Charter-Considerations.doc">Charter Considerations</a></li>
-            <li><a href="https://www.cdfifund.gov/programs-training/certification/cdfi">CDFI Certification</a></li>
-            <li><a href="https://lscu.coop/member-engagement/docs/Conflict-of-Interest-Resolution-0920.pdf">Conflict of Interest Resolution</a></li>
+            {list.map((item) => (
+              <li key={item.href}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
           </ul>
         </div>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
 export default AdditionalResources;
--- app/components/sections/FreeTraining.tsx
+++ app/components/sections/FreeTraining.tsx
@@ -3,29 +3,16 @@ import Section from "../layout/Section";
 import TwoCol from "../layout/TwoCol";
 import VideoContainer from "../VideoContainer";
 import { ContentProps } from "../utils/sectionProps";
-import Link from "../Link"
+import Link from "../Link";
 
 const FreeTraining = ({ bgColor }: ContentProps) => {
   return (
     <Section bgColor={bgColor}>
-      <div className="tw-font-bold tw-italic tw-text-center tw-text-2xl md:tw-text-3xl lg:tw-text-5xl tw-pb-10 tw-text-brandingBlue-100">
-        Free Training
-      </div>
       <TwoCol>
         <VideoContainer src="https://www.youtube.com/embed/5_n1zxf3EjM" />
-        <div className="tw-space-y-4 tw-mx-auto">
-          <div>
-            <Link href="https://www.fincen.gov/">FinCen.gov</Link>
-          </div>
-          <div>
-            <Link href="https://www.fincen.gov/frequently-asked-questions-regarding-fincen-currency-transaction-report-ctr">FinCen CTR FAQs</Link>
-          </div>
-          <div>
-            <Link href="https://bsaaml.ffiec.gov/manual">FFIEC, BSA Manual</Link>
-          </div>
-        </div>
-      </TwoCol >
-    </Section >
+        <div className="tw-space-y-4 tw-mx-auto"></div>
+      </TwoCol>
+    </Section>
   );
 };
 

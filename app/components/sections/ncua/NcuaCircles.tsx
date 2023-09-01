86da53bc0305364292a12af0d9885cc7ee70648c
dillonzellis <dillonzellis@gmail.com>
2023-09-01 11:12:11 -0400

    edit ncua circles order by key

--- app/components/sections/ncua/NcuaCircles.tsx
+++ app/components/sections/ncua/NcuaCircles.tsx
@@ -1,4 +1,3 @@
-
 import Container from "../../layout/Container";
 import Circle from "../../circles/Circle";
 import Section from "../../layout/Section";
@@ -7,24 +6,34 @@ import { Color } from "../../utils/colorClasses";
 
 type NcuaCirclesProps = {
   bgColor: Color;
-}
+};
 
 const NcuaCircles = ({ bgColor }: NcuaCirclesProps) => {
   return (
     <Section bgColor={bgColor}>
       <div className="tw-mx-auto tw-flex tw-justify-center tw-pb-8">
-        <img className="tw-w-[400px]" src="http://dev2.growthbydesign.org/wp-content/uploads/ncua.jpg" />
+        <a href="https://ncua.gov/" target="_blank">
+          <img
+            className="tw-w-[400px]"
+            src="http://dev2.growthbydesign.org/wp-content/uploads/ncua.jpg"
+          />
+        </a>
       </div>
 
       <Container>
         <div className="tw-flex tw-flex-wrap tw-justify-evenly">
           {ncuaData.map((data, idx) => (
-            <Circle key={idx} title={data.title} link={data.link} variant="light" />
+            <Circle
+              key={idx}
+              title={data.title}
+              link={data.link}
+              variant="light"
+            />
           ))}
         </div>
       </Container>
-    </Section >
-  )
-}
+    </Section>
+  );
+};
 
 export default NcuaCircles;

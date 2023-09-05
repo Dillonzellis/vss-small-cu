955f3f211e6f4e0649753d44b52c021967ca6581
dillonzellis <dillonzellis@gmail.com>
2023-09-05 11:57:51 -0400

    make training vids youtube vids

--- app/components/sections/more-training-vids/data.tsx
+++ app/components/sections/more-training-vids/data.tsx
@@ -2,7 +2,7 @@ export const trainingVids = [
   {
     key: 1,
     title: "Cybersecurity Training",
-    src: "https://www.youtube.com/embed/gg5gPN1aTP0",
+    src: "https://www.youtube.com/embed/gg5gPN1aTP0?modestbranding=1&showsearch=0",
   },
   {
     key: 2,
@@ -22,26 +22,26 @@ export const trainingVids = [
   {
     key: 5,
     title: "Board Responsibilities ",
-    src: "https://lscu.coop/member-engagement/docs/vids_0820/bd_resp_0820.mp4",
+    src: "https://www.youtube.com/embed/pci4ptTN8YE?si=YXDd0Bv3DkNqew19",
   },
   {
     key: 6,
     title: "Supervisory Committee ",
-    src: "https://lscu.coop/member-engagement/docs/vids_0820/supv_comm_0820.mp4",
+    src: "https://www.youtube.com/embed/LpxIlExE2Ic?si=jvvXVqKWo8QqDIJX",
   },
   {
     key: 7,
     title: "Understanding CU Financials ",
-    src: "https://lscu.coop/member-engagement/docs/vids_0820/cu_financials_0820.mp4",
+    src: "https://www.youtube.com/embed/sfyosbX_kmY?si=xolsEFKec-gcK_D2",
   },
   {
     key: 8,
     title: "Member Service",
-    src: "https://lscu.coop/member-engagement/docs/zoom_9.mp4",
+    src: "https://www.youtube.com/embed/3-GdmIsAYCY?si=anc5liHMou2tZA0s",
   },
   {
     key: 9,
     title: "Interest Rate Risk ",
-    src: "https://lscu.coop/member-engagement/docs/interest-rate-risk.mp4",
+    src: "https://www.youtube.com/embed/kxVBeVPpu8M?si=-QSyeYK53dpFIlUK",
   },
 ];

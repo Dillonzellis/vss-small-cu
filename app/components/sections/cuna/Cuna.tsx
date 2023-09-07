de198e2ccf37d39c8d303ea636e4d3499713349c
dillonzellis <dillonzellis@gmail.com>
2023-09-07 13:47:17 -0400

    update cuna logo path

--- app/components/sections/cuna/Cuna.tsx
+++ app/components/sections/cuna/Cuna.tsx
@@ -1,4 +1,3 @@
-
 import Container from "../../layout/Container";
 import Circle from "../../circles/Circle";
 import Section from "../../layout/Section";
@@ -7,23 +6,31 @@ import { Color } from "../../utils/colorClasses";
 
 type CunaCirclesProps = {
   bgColor: Color;
-}
+};
 
 const CunaCircles = ({ bgColor }: CunaCirclesProps) => {
   return (
     <Section bgColor={bgColor}>
       <div className="tw-mx-auto tw-flex tw-justify-center tw-pb-8">
-        <img className="tw-p-4 tw-rounded-md " src="http://dev2.growthbydesign.org/wp-content/uploads/CUNA-Logo.png" />
+        <img
+          className="tw-p-4 tw-rounded-md "
+          src="https://lscu.coop/member-engagement/imgs/CUNA-Logo.png"
+        />
       </div>
       <Container>
         <div className="tw-gap-2 tw-flex tw-flex-wrap tw-justify-evenly">
           {cunaData.map((data, idx) => (
-            <Circle key={idx} title={data.title} link={data.link} variant="dark" />
+            <Circle
+              key={idx}
+              title={data.title}
+              link={data.link}
+              variant="dark"
+            />
           ))}
         </div>
       </Container>
-    </Section >
-  )
-}
+    </Section>
+  );
+};
 
 export default CunaCircles;

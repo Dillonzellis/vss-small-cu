e11b562b11f30c5363cea8be0843a6de82f699d0
dillonzellis <dillonzellis@gmail.com>
2023-10-17 11:50:18 -0400

    new videos and thumbnails

--- app/components/sections/more-training-vids/MoreTraining.tsx
+++ app/components/sections/more-training-vids/MoreTraining.tsx
@@ -7,7 +7,7 @@ const MoreTraining = ({ bgColor }: ContentProps) => {
     <Section id="other-trainings" bgColor={bgColor}>
       <div className="tw-items-end tw-justify-center tw-gap-8 md:tw-grid md:tw-grid-cols-3">
         {trainingVids.map((vid) => (
-          <div key={vid.key} className="tw-pb-8 md:tw-pb-4">
+          <div key={vid.src} className="tw-pb-8 md:tw-pb-4">
             <div>
               <iframe
                 className="tw-aspect-video tw-shadow-lg tw-shadow-brandingBlue-700/40"
--- app/components/sections/more-training-vids/data.tsx
+++ app/components/sections/more-training-vids/data.tsx
@@ -1,37 +1,38 @@
 export const trainingVids = [
   {
-    key: 1,
+    title: "BSA Training",
+    src: "https://www.youtube.com/embed/5_n1zxf3EjM",
+  },
+  {
     title: "Cybersecurity Training",
     src: "https://www.youtube.com/embed/gg5gPN1aTP0?modestbranding=1&showsearch=0",
   },
   {
-    key: 3,
-    title: "Robbery Training",
+    title: "Fair Lending",
+    src: "",
+  },
+  {
+    title: "Robbery Awareness Training",
     src: "https://www.youtube.com/embed/xPBY_LaAxwc?si=MJYtQl8vRStfR7kh",
   },
   {
-    key: 5,
+    title: "Elder Financial Exploitation",
+    src: "",
+  },
+  {
     title: "Board Responsibilities ",
     src: "https://www.youtube.com/embed/Ba3QS7sb4sY?si=gINJSlPtHtEjeWRr",
   },
   {
-    key: 6,
-    title: "Supervisory Committee ",
+    title: "Supervisory Committee Training",
     src: "https://www.youtube.com/embed/6V0FNGG7ohs?si=i3OHRZFWDAA5Iqi8",
   },
   {
-    key: 7,
-    title: "Understanding CU Financials ",
+    title: "CU Financials",
     src: "https://www.youtube.com/embed/PQymAsSUHgs?si=Xfjuzb8fPqeubfq4",
   },
   {
-    key: 8,
     title: "Member Service",
     src: "https://www.youtube.com/embed/3-GdmIsAYCY?si=anc5liHMou2tZA0s",
-  },
-  {
-    key: 9,
-    title: "BSA Training",
-    src: "https://www.youtube.com/embed/5_n1zxf3EjM",
   },
 ];
